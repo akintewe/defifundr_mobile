ab20094bdb52727a00ef401ed63a79062b2e70c2
hoossayn <abdulhakeemhusseni@gmail.com>
2025-02-23 20:17:29 +0100

    feat: base app set up

--- lib/app.dart
+++ lib/app.dart
@@ -1,12 +1,59 @@
 import 'package:flutter/material.dart'
-    show BuildContext, MaterialApp, StatelessWidget, Widget;
+    show BouncingScrollPhysics, BuildContext, Colors, MaterialApp, ScrollBehavior, ScrollPhysics, StatelessWidget, ThemeData, Widget;
+import 'package:flutter/services.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
-class App extends StatelessWidget {
+import ' core/routers/_routes.dart';
+
+class App extends ConsumerStatefulWidget {
   const App({super.key});
 
   @override
-  Widget build(BuildContext context) => const MaterialApp(
+  ConsumerState<App> createState() => _AppState();
+
+}
+
+class _AppState extends ConsumerState<App> {
+  @override
+  void initState() {
+    SystemChrome.setSystemUIOverlayStyle(
+      const SystemUiOverlayStyle(
+        statusBarIconBrightness: Brightness.light,
+        statusBarBrightness: Brightness.light,
+        systemNavigationBarIconBrightness: Brightness.light,
+      ),
+    );
+
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ScreenUtilInit(
+      designSize: const Size(410, 890),
+      minTextAdapt: true,
+      splitScreenMode: false,
+      child: MaterialApp.router(
         debugShowCheckedModeBanner: false,
-        
-      );
+        title: 'DeFiFundr',
+        scrollBehavior: const _AppScrollBehavior(),
+        theme: ThemeData(
+          primarySwatch: Colors.grey,
+        ),
+        routeInformationProvider: AppRouter.router.routeInformationProvider,
+        routeInformationParser: AppRouter.router.routeInformationParser,
+        routerDelegate: AppRouter.router.routerDelegate,
+      ),
+    );
+  }
+}
+
+class _AppScrollBehavior extends ScrollBehavior {
+  const _AppScrollBehavior();
+
+  @override
+  ScrollPhysics getScrollPhysics(BuildContext context) {
+    return const BouncingScrollPhysics();
+  }
 }
---
+++ lib/feature/create_password/create_password_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CreatePasswordScreen extends StatefulWidget {
+  const CreatePasswordScreen({Key? key}) : super(key: key);
+
+  @override
+  State<CreatePasswordScreen> createState() => _CreatePasswordScreenState();
+}
+
+class _CreatePasswordScreenState extends State<CreatePasswordScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
