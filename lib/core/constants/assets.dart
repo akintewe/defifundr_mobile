5d21e60331bc988b87b2d2148ee9773f69fd82c7
Olamiposi <tundexsmith624@gmail.com>
2025-02-28 19:34:05 +0000

    created verify nin screen

--- lib/core/constants/assets.dart
+++ lib/core/constants/assets.dart
@@ -14,6 +14,8 @@ class AppAssets {
   static String downArrow = '${defifundrIconPath}arrow_down.svg';
   static String backgroundImage = '${defifundrIconPath}background.png';
   static String bvnSuffixIcon = '${defifundrIconPath}bvn_suffix.svg';
+  static String ninSlipImage = '${defifundrIconPath}/nin_slip.png';
+  static String arrowRightCurve = '${defifundrIconPath}arrow_right.svg';
 
   //-------------------------------------------- IMAGES -------------------------------------------- //
   static const defifundrImagePath = 'assets/images';
